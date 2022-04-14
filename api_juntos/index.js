16eafdf6ff782f466b724973ab38a47130226aef
EmanuelAugusto <emanue02augusto@gmail.com>
2022-04-14 19:54:16 -0300

    adjust

--- api_juntos/index.js
+++ api_juntos/index.js
@@ -2,8 +2,8 @@ import Express from "express";
 import Routes from "./src/Routes/Routes.js";
 import DotEnv from "dotenv";
 import Db from "./src/Database/Database.js";
-import ModelsSync from "./src/Database/ModelsSync.js"
-import Cors from "cors"
+import ModelsSync from "./src/Database/ModelsSync.js";
+import Cors from "cors";
 
 DotEnv.config();
 
@@ -11,16 +11,16 @@ const App = Express();
 
 App.use(Cors());
 App.use(Express.json());
-App.use(Routes);
+App.use("/ongs", Routes);
 
-const RunApp = async () => {
-  try {
-    await Db.authenticate();
-    ModelsSync();
-    App.listen(process.env.PORT);
-  } catch (error) {
-    console.log(error);
-  }
+const RunApp = async() => {
+    try {
+        await Db.authenticate();
+        ModelsSync();
+        App.listen(process.env.PORT);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 RunApp();
